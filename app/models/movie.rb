804e4e17d188bd221e13258d85a01acc74bbea62
Amen Atikpo <alaskale2024@gmail.com>
2022-09-05 12:09:33 +0000

    commit tree c28940a9b9c3aa838d4372219e3ac9f93f2c1fd9

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,31 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+    def self.find_movies_after_2002
+        Movie.where("release_date>2002")
+    end
+    def update_with_attributes(attribute1,attribute2)
+        movie = Movie.find_by(attribute1)
+        movie.update(title: attribute2)
+    end
+
+    
+
 
 end
